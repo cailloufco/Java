ba572567eea33b7aab8f34e6a30af81be938383c
Caio <caioextreme000@gmail.com>
2026-09-09 11:52:17 -0300

    realizando Atividade_3 - Tratamento de Erros e Exceções (não concluida)

--- Estudos/src/POO/metodos/App.java
+++ Estudos/src/POO/metodos/App.java
@@ -6,7 +6,6 @@ public class App {
         Atributos p1Atributos = new Atributos(2600 , 256 , 734 , 360);
         Personagem p1 = new Personagem("FenixAlq" , 256 , "Alquimista" , p1Atributos);
 
-
         Atributos p2Atributos = new Atributos(10730 , 1200 , 529, 2366);
         Personagem p2 = new Personagem("Evokee" , 529 , "Guerreiro" , p2Atributos);
 
